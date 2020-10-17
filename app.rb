1f77ff4b6cc9e33ecb4a55c1e6474fb689ea2046
sergiocardenasg <sergiocardenas@utexas.edu>
2020-10-17 10:38:23 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Sergio"
+    end
+
+    get '/hometown' do
+        "My hometown is Ciudad Juarez"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Berenstein by The Band CAMINO"
+    end
 end
